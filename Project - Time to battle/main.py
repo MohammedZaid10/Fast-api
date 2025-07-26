e3c1b5301d3a51e6ff2d20bf8a2183a51639b26f
MohammedZaid10 <mohammedzaid1012@gmail.com>
2025-07-26 11:33:29 +0530

    commit

--- Project - Time to battle/main.py
+++ Project - Time to battle/main.py
@@ -1,9 +1,12 @@
 from Zombie import *
+from Ogre import *
 
 # big_zombie = Enemy("Big Zombie", 100, 10)
 # print(big_zombie.health_points)
 # print(big_zombie.attack())
 
 zombie = Zombie(10, 1)
+ogre = Ogre(20, 3)
 
-print(zombie.spread_disease())
+zombie.spread_disease()
+ogre.talk()
