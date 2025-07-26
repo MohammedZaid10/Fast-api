b4d34d2676a62a550e5525cf65afa33c4c7580d2
MohammedZaid10 <mohammedzaid1012@gmail.com>
2025-07-26 12:29:36 +0530

    battle

--- Project - Time to battle/Zombie.py
+++ Project - Time to battle/Zombie.py
@@ -1,4 +1,5 @@
 from Enemy import *
+import random
 
 
 class Zombie(Enemy):
@@ -11,3 +12,9 @@ class Zombie(Enemy):
 
     def spread_disease(self):
         print("Zombie is trying to spread disease...")
+
+    def special_attack(self):
+        did_special_attack_work = random.random() < 0.50
+        if did_special_attack_work:
+            self.health_points += 2
+            print("Zombie regenerated 2HP!")
