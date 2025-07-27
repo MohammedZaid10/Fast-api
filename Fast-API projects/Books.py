6fd9ced29f503ec28fdc1bc18383f74fdd4b5091
MohammedZaid10 <mohammedzaid1012@gmail.com>
2025-07-27 08:26:48 +0530

    Lesson 71 - FastAPI Project: Path Parameters

--- Fast-API projects/Books.py
+++ Fast-API projects/Books.py
@@ -15,3 +15,20 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+# Always make sure that an API with a static parameter comes before a dynamic parameter
+# @app.get("/books/mybook")
+# async def read_all_books():
+#     return {'book title': 'My favourite book'}
+
+
+# @app.get("/books/{dynamic_param}")
+# async def read_all_books(dynamic_param: str):
+#     return {'dynamic_param': dynamic_param}
+
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
